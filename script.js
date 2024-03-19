4a360a7f1d30c0c5a434def050a864bf837ef97e
Talika Bajaj <bajajtalika2001@gmail.com>
2024-03-19 21:28:11 +0530

    Add setInterval for rotation of arms

---
+++ script.js
@@ -0,0 +1,18 @@
+
+setInterval(()=> {
+        let date = new Date();
+        let hr = date.getHours();
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
+        
+        let hRotate = 30*hr + min/2;
+        let mRotate = 6*min;
+        let sRotate = 6*sec;
+
+        let h = document.getElementById("hr");
+        h.style.transform = `rotate(${hRotate}deg)`
+        let m = document.getElementById("min");
+        m.style.transform = `rotate(${mRotate}deg)`
+        let s = document.getElementById("sec");
+        s.style.transform = `rotate(${sRotate}deg)`
+    }, 1000)
