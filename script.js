72f53d2f2859af5b6b709a336f71ed592bddb4ea
Talika Bajaj <bajajtalika2001@gmail.com>
2024-03-19 22:58:31 +0530

    Add styles

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-//setInterval to move clock arms
-setInterval(()=> {
-        let date = new Date();
-        let hr = date.getHours();
-        let min = date.getMinutes();
-        let sec = date.getSeconds();
-        
-        let hRotate = 30*hr + min/2;
-        let mRotate = 6*min;
-        let sRotate = 6*sec;
+setInterval(() => {
+	d = new Date(); //object of date()
+	hr = d.getHours();
+	min = d.getMinutes();
+	sec = d.getSeconds();
+	hr_rotation = 30 * hr + min / 2; //converting current time
+	min_rotation = 6 * min;
+	sec_rotation = 6 * sec;
 
-        let h = document.getElementById("hr");
-        h.style.transform = `rotate(${hRotate}deg)`
-        let m = document.getElementById("min");
-        m.style.transform = `rotate(${mRotate}deg)`
-        let s = document.getElementById("sec");
-        s.style.transform = `rotate(${sRotate}deg)`
-    }, 1000)
+	hour.style.transform = `rotate(${hr_rotation}deg)`;
+	minute.style.transform = `rotate(${min_rotation}deg)`;
+	second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
